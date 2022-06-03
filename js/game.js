09e138dfd29648029cf36009edae101bb8bddf6d
laszlo-ratesic <88006245+laszlo-ratesic@users.noreply.github.com>
2022-06-03 11:49:34 -0500

    define bullet

--- js/game.js
+++ js/game.js
@@ -98,6 +98,48 @@ const Turret = new Phaser.Class({
   },
 });
 
+const Bullet = new Phaser.Class({
+  Extends: Phaser.GameObjects.Image,
+
+  initialize: function Bullet(scene) {
+    Phaser.GameObjects.Image.call(this, scene, 0, 0, 'bullet');
+
+    this.dx = 0;
+    this.dy = 0;
+    this.lifespan = 0;
+
+    this.speed = Phaser.Math.GetSpeed(600, 1);
+  },
+
+  fire: function (x, y, angle) {
+    this.setActive(true);
+    this.setVisible(true);
+
+    // Bullets fire from the middle of the screen to x and y above
+    this.setPosition(x, y);
+
+    // we dont need to rotate the bullets as they are round
+    // this.setRotation(angle);
+
+    this.dx = Math.cos(angle);
+    this.dy = Math.sin(angle);
+
+    this.lifespan = 300;
+  },
+
+  update: function (time, delta) {
+    this.lifespan -= delta;
+
+    this.x += this.dx * (this.speed * delta);
+    this.y += this.dy * (this.speed * delta);
+
+    if (this.lifespan <= 0) {
+      this.setActive(false);
+      this.setVisible(false);
+    }
+  },
+});
+
 function drawGrid(graphics) {
   graphics.lineStyle(1, 0x0000ff, 0.8);
   for (let i = 0; i < 8; i++) {
